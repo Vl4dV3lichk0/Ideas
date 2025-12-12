a587691df4b3d7de4a3640dee75a3e57eb321059
Vlad Velichko <vladvelichko2004@icloud.com>
2025-12-12 17:28:06 +0300

    Package imports corrected, crud.py is initialised

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "0.0.1"
 
-from .connection import connect
+from .connection import DatabaseConnection
+from .crud import CategoryCRUD, IdeaCRUD
+from .models import Category, Idea
 
-__all__ = ['connect']
+__all__ = ['DatabaseConnection', 'CategoryCRUD', 'IdeaCRUD', 'Category', 'Idea']
