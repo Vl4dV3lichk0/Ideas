27a5e8bbcdb2f3f185a2b8794eb4505f4f41af3e
Vlad Velichko <vladvelichko2004@icloud.com>
2025-12-19 16:28:36 +0300

    Basic logics is done. UI is initialised.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Ideas")
+
+
 if __name__ == "__main__":
-    pass
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()  
+    app.exec()
